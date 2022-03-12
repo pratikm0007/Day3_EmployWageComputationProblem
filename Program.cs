cc4a26338ae52bb6f74dea24398cfa3c3a62f478
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-12 17:22:16 +0530

    Push UC1 file

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace EmployWageComputationProblem
+{
+    class Program
+    {
+        /// <summary>
+        /// Check Employ is Absent or Present
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int forFullHrsEmploy = 1;
+
+            //Computation to get Random value 0 & 1.
+            Random random = new Random();
+            int value = random.Next(2);
+
+            if (value == forFullHrsEmploy)
+            {
+                Console.WriteLine("Employ is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employ is Absent");
+            }
+        }
+    }
+}
