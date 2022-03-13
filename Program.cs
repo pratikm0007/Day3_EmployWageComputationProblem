d5b0e040f1e2dd6a2e52cf2f1955806248f3cf09
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-13 09:56:16 +0530

    Push UC6 file

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace EmployWageComputationProblem
+{
+    class Program
+    {
+        /// <summary>
+        /// Calculate Wage Till condition of 100 Hours or 20 Day meets.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            const int forFullHrsEmploy = 1;
+            const int forPartTimeHrsEmploy = 0;
+            const int wagePerHrs = 20;
+            const int workDayInMonth = 20;
+            const int workHrsInMonth = 100;
+
+            int hrs = 0;
+            int totalEmployhrs = 0;
+            int totalWorkDayInMonth = 0;
+            int totalEmploywage = 0;
+
+            while (totalWorkDayInMonth < workDayInMonth && totalEmployhrs < workHrsInMonth)
+            {
+                totalWorkDayInMonth++;
+                //Computation to get Random value 0 & 1.
+                Random random = new Random();
+                int value = random.Next(3);
+
+                switch (value)
+                {
+                    case forFullHrsEmploy:
+                        Console.WriteLine("Employ is FullTime Present");
+                        hrs = 8;
+                        break;
+                    case forPartTimeHrsEmploy:
+                        Console.WriteLine("Employ is PartTime Present");
+                        hrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employ is Absent");
+                        hrs = 0;
+                        break;
+                }
+                totalEmployhrs+= hrs;
+                Console.WriteLine("Day : {0} , Employ Hours : {1} ",+totalWorkDayInMonth,totalEmployhrs);
+                
+            }
+            totalEmploywage = totalEmployhrs * wagePerHrs;
+            Console.WriteLine("Employ  wage is : {0}", +totalEmploywage);
+
+            
+        }
+    }
+}
