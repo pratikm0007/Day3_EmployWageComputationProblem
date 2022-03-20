9750048168c7907b86b7238a055cc9f9da5873a1
pratikm0007 <pratikpmondal07@gmail.com>
2022-03-20 13:18:27 +0530

    Push Refactor file

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace EmployWageComputationProblem
+{
+    class Program
+    {
+        /// <summary>
+        /// Calculate Monthly Wage
+        /// </summary>
+        static void CalMonthlyWage()
+        {
+            const int forFullHrsEmploy = 1;
+            const int forPartTimeHrsEmploy = 0;
+            const int wagePerHrs = 20;
+            const int workDayInMonth = 20;
+
+            int hrs = 0;
+            int empWage = 0;
+            int monthWage = 0;
+
+            for (int day = 0; day < workDayInMonth; day++)
+            {
+                //Computation to get Random value 0 & 1.
+                Random random = new Random();
+                int value = random.Next(3);
+
+                switch (value)
+                {
+                    case forFullHrsEmploy:
+                        Console.WriteLine("Employ is FullTime Present");
+                        hrs = 8;
+                        break;
+                    case forPartTimeHrsEmploy:
+                        Console.WriteLine("Employ is PartTime Present");
+                        hrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employ is Absent");
+                        hrs = 0;
+                        break;
+                }
+                empWage = wagePerHrs * hrs;
+                Console.WriteLine("Employ wage is : {0}", +empWage);
+                monthWage += empWage;
+            }
+            Console.WriteLine("Employ Monthly wage is : {0}", +monthWage);
+        }
+        static void Main(string[] args)
+        {
+
+            Program.CalMonthlyWage();
+
+        }
+    }
+}
